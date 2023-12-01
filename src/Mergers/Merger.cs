6b0766f34e90b242644984e5009b3063ee453e1c
海棠云螭 <haitangyunchi@126.com>
2023-12-01 20:09:17 +0800

    添加项目文件。

---
+++ src/Mergers/Merger.cs
@@ -0,0 +1,19 @@
+﻿namespace GICutscenes.Mergers
+{
+    internal interface Merger
+    {
+
+        void AddVideoTrack(string videoFile);
+
+        void AddAudioTrack(string audioFile, int lang);
+
+        void AddSubtitlesTrack(string subFile, string language);
+
+        void AddAttachment(string attachment, string description);
+
+        void Merge();
+
+        void Merge(string audioFormat, string videoFormat) { }
+
+    }
+}
